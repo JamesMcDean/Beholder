f208df3556dd34ad7f87ef164adb600ed1361fbe
James McRoberts <jamesmcroberts82997@gmail.com>
2019-12-21 20:46:06 +0000

    I guess I completely forgot that the robot has guns.

--- src/Control/robotcontol.h
+++ src/Control/robotcontol.h
@@ -1,9 +1,18 @@
 #ifndef GENERAL_ROBOT_CONTROL_H
 #define GENERAL_ROBOT_CONTROL_H
 
+typedef uint8_t FIRING_FLAGS;
+
+enum firingFlags {
+    FIRING_none = 0,
+
+    FIRING_primary = 1u << 0u,
+    FIRING_secondary = 1u << 2u
+};
+
 /**
  * Holds all different control states for a robot.
- * All states are in the rand [-1.0, 1.0]
+ * All states are in the range [-1.0, 1.0].
  */
 typedef struct {
     float base_forward;             // +: Forward; -: Backward
@@ -15,6 +24,8 @@ typedef struct {
 
     float secondaryGun_pitch;       // +: Up; -: Down
     float secondaryGun_rotation;    // +: Clockwise; -: Counter-Clockwise
+
+    FIRING_FLAGS firing;
 } ROBOT_CONTROL_BULK;
 
 #endif
