d7532422640b0e9695e88969cfe8b5ae053f829b
James McRoberts <jamesmcroberts82997@gmail.com>
2019-12-21 20:47:23 +0000

    Added documentation and methods to control the robot with controller states.

--- src/Control/robot.cpp
+++ src/Control/robot.cpp
@@ -12,12 +12,19 @@ namespace Control {
                                                         + "the device at \"" + devicePath + "\" with the speed of "
                                                         + std::to_string(baud) + " baud.");
 
-        _cameras = cameras;
-        if (useResponseChar) __responseChar = responseChar;
+        _cameras = std::move(cameras);
+        if (useResponseChar) __responseChar = std::make_unique<char>(responseChar);
+    }
+
+    auto Robot::__currentTime() -> std::chrono::milliseconds {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now().time_since_epoch()
+                );
     }
 
     auto Robot::__updateState() -> void {
         char* structTape = static_cast<char*>(static_cast<void*>(&_robotState));
+        serialPutchar(__fd, Config::SERIAL_STATE_CHAR);
         serialPuts(__fd, structTape);
         delete[] structTape;
     }
@@ -53,9 +60,9 @@ namespace Control {
 
     auto Robot::isConnected() -> bool {
         // TEST
-        serialPutchar(__fd, SERIAL_TEST_CHAR);
+        serialPutchar(__fd, Config::SERIAL_TEST_CHAR);
         serialPuts(__fd, "OK");
-        std::this_thread::sleep_for(std::chrono::milliseconds(SERIAL_WAIT_TIME_MILLIS));
+        std::this_thread::sleep_for(std::chrono::milliseconds(Config::SERIAL_RESPONSE_TIME_MILLIS));
 
         // ACK
         if (serialDataAvail(__fd) && (__responseChar == nullptr || serialGetchar(__fd) == *__responseChar)) {
@@ -80,6 +87,39 @@ namespace Control {
     }
 
     auto Robot::updateControllerState(const GAME_CONTROLLER_BULK& data) -> void {
-        
+        // Running data
+        const std::chrono::milliseconds updateTime = __currentTime();
+        const float durationRaw = (float) updateTime.count() - _lastUpdate.count();
+        const float speed = (float) Config::ROBOT_SPEED_PER_SEC / 1000;
+
+        /*
+         * Set rotations
+         * TODO - Implement targeting.
+         *
+         * note: Using the previous state of the controller for proper reaction to duration.
+         */
+        const float bodyRot = _robotState.base_rotation + (speed * durationRaw * _controllerState.rightStickX);
+        const float aimPit_p = _robotState.primaryGun_pitch + (speed * durationRaw * _controllerState.rightStickY);
+        const float aimPit_s = _robotState.secondaryGun_pitch + (speed * durationRaw * _controllerState.rightStickY);
+
+        _robotState.base_rotation = bodyRot;
+        _robotState.primaryGun_pitch = aimPit_p; // TODO
+        _robotState.secondaryGun_pitch = aimPit_s; // TODO
+        _robotState.primaryGun_rotation = 0; // TODO
+        _robotState.secondaryGun_rotation = 0; // TODO
+
+        // Set movement
+        _robotState.base_forward = data.leftStickY;
+        _robotState.base_strafe = data.leftStickX;
+
+        /*
+         * Set firing states
+         * note: Using the most recent state of the controller for proper firing response.
+         */
+        _robotState.firing = data.rightTrigger > Config::TRIGGER_FIRING_THRESHOLD ? FIRING_primary : FIRING_none;
+        _robotState.firing |= data.leftTrigger > Config::TRIGGER_FIRING_THRESHOLD ? FIRING_secondary : FIRING_none;
+
+        // Send state
+        __updateState();
     }
 }
--- src/Control/robot.hpp
+++ src/Control/robot.hpp
@@ -7,6 +7,8 @@
 
 #include <vector>
 #include <string>
+#include <chrono>
+#include <thread>
 
 #include "robotcontol.h"
 #include "gamecontroller.h"
@@ -26,17 +28,36 @@ namespace Control {
         int __fd;
         std::unique_ptr<char> __responseChar = nullptr;
 
+        /**
+         * The backend for the constructors.
+         * @param devicePath The path that the serial device exists at to communicate with the robot.
+         * @param baud The baud rate of the serial connection.
+         * @param cameras The cameras to bind to the robot controller.
+         * @param responseChar The char to check for after sending a test message.
+         * @param useResponseChar If the class should check for a specific response char when testing the connection.
+         */
         auto __init(const std::string& devicePath, int baud, std::vector<std::shared_ptr<Vision::Camera>> cameras,
                 char responseChar, bool useResponseChar) -> void;
 
+        /**
+         * Gets the current milliseconds since January 1st, 1970.
+         * @return The milliseconds since January 1st, 1970.
+         */
+        static auto __currentTime() -> std::chrono::milliseconds;
+
+        /**
+         * Sends the state contained in _robotState to the robot over serial.
+         */
         auto __updateState() -> void;
 
     protected:
         std::vector<std::shared_ptr<Vision::Camera>> _cameras;
         STREAM_CONTROL_DATA _streamData; // Row major
-        GAME_CONTROLLER_BULK _controlState;
-        ROBOT_CONTROL_BULK _robotState;
         uint8_t* _previousFrame;
+
+        GAME_CONTROLLER_BULK _controllerState = {0};
+        ROBOT_CONTROL_BULK _robotState = {0};
+        std::chrono::milliseconds _lastUpdate = __currentTime();
 
     public:
         Robot(const std::string& devicePath, int baud, const std::shared_ptr<Vision::Camera>& camera);
@@ -49,10 +70,37 @@ namespace Control {
 
         ~Robot();
 
+        /**
+         * Checks if the robot is connected to the serial connection optionally using the responseChar provided
+         * during construction.
+         * @return If the robot is connected.
+         */
         auto isConnected() -> bool;
+
+        /**
+         * Get the data to send over the stream.
+         * @return The data to send over the stream.
+         */
         auto getStreamData() -> STREAM_CONTROL_DATA;
+
+        /**
+         * Gets the raw frame of the stream to send over the stream.
+         * @return The raw frame of the stream.
+         */
         auto getStreamFrame() -> uint8_t*;
+
+        /**
+         * Gets the most recently captured frame from the camera at the index provided.
+         * @param index The index of the camera (from the camera vector) to query.
+         * @return The OpenCV matrix defining the frame from the camera.
+         */
         auto getCameraFrame(size_t index) -> cv::Mat;
+
+        /**
+         * Uses the controller state provided (mixed with the most recent update state and time) to control the
+         * absolute rotation of the robot's guns along with the other directly mapped states.
+         * @param data The controller data to use for the update.
+         */
         auto updateControllerState(const GAME_CONTROLLER_BULK& data) -> void;
     };
 }
