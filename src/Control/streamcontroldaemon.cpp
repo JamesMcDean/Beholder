656ebd16c6656e7f926fad5b58a1c88b45c60370
James McRoberts <jamesmcroberts82997@gmail.com>
2020-01-02 02:14:25 +0000

    Made network communication more robust

--- src/Control/streamcontroldaemon.cpp
+++ src/Control/streamcontroldaemon.cpp
@@ -3,67 +3,127 @@
 //
 
 #include "streamcontroldaemon.hpp"
-#include "math.cpp"
 
 namespace Control {
-    StreamControlDaemon::StreamControlDaemon(ROBOT_ID id, uint16_t videoPort, uint16_t controlPort, bool start)
-    : bot(id), videoPort(videoPort), controlPort(controlPort) {
-        running = std::make_shared<bool>(start);
-        if (start) __workerThread = std::thread(workerMain, bot, videoPort, controlPort, running);
+    StreamControlDaemon::StreamControlDaemon(const std::shared_ptr<Robot>& bot, uint16_t streamPort, uint16_t controlPort, bool start)
+    : _bot(bot), _streamPort(streamPort), _controlPort(controlPort) {
+        __running = std::make_shared<bool>(start);
+        if (start) __workerThread = std::thread(__workerMain, bot, streamPort, controlPort,
+                __running, _stopReason, _stopException);
     }
 
     StreamControlDaemon::~StreamControlDaemon() {
-        *running = false;
+        *__running = false;
         if (__workerThread.joinable()) __workerThread.join();
     }
 
-    auto StreamControlDaemon::workerMain(ROBOT_ID bot, uint16_t videoPort, uint16_t controlPort,
-            const std::shared_ptr<bool>& active) -> void {
+    auto StreamControlDaemon::__workerMain(const std::shared_ptr<Robot>& bot, uint16_t videoPort, uint16_t controlPort,
+                                           const std::shared_ptr<bool>& active, const std::shared_ptr<std::string>& stopMsg,
+                                           const std::shared_ptr<std::exception>& errPtr) -> void {
         using namespace boost::asio;
         using ip::tcp;
         using ip::udp;
 
-        boost::asio::io_service service;
+        // Running data
+        GAME_CONTROLLER_BULK controlData;
+        boost::system::error_code err;
 
-        tcp::acceptor controlAcceptor(service, tcp::endpoint(tcp::v4(), REMOTE_CONTROL_PORT));
-        tcp::socket controlSocket(service);
-        controlAcceptor.accept(controlSocket);
+        try {
+            // Set up connections
+            boost::asio::io_service service;
+            tcp::socket controlSocket(service);
+            tcp::acceptor controlAcceptor(service, tcp::endpoint(tcp::v4(), controlPort));
 
-        while (*active) {
-            GAME_CONTROLLER_BULK controlData = socketRead(controlSocket);
-            Control::updateRobotState(bot, );
+            udp::socket streamFrameSocket(service, udp::endpoint(udp::v4(), videoPort));
+            udp::endpoint streamEndpoint;
+            {
+                boost::array<uint8_t, 1> rcvBuf{};
+                streamFrameSocket.receive_from(buffer(rcvBuf), streamEndpoint);
+            }
+
+            // Exchange data
+            while (*active && !err) {
+                auto controlResult = __controlRead(controlSocket);
+                controlData = std::get<0>(controlResult);
+                err = std::get<1>(controlResult);
+                if (err || !*active) break;
+                bot->updateControllerState(controlData);
+
+                err = __controlWrite(controlSocket, bot->getStreamData());
+                if (err || !*active) break;
+                err = __videoWrite(streamFrameSocket, streamEndpoint, bot->getStreamData(), bot->getStreamFrame());
+            }
+        }
+        catch (std::exception& ex) {
+            *active = false;
+
+            *stopMsg = "Exception: ";
+            *stopMsg += ex.what();
+            *errPtr = ex;
+        }
+
+        if (err) {
+            *active = false;
+
+            *stopMsg = err.message();
+            //             MESSAGE                 ' '  (   ERROR_VALUE                            )  '\0'
+            char msgBuffer[err.message().length() + 1 + 1 + std::to_string(err.value()).length() + 1 + 1];
+            sprintf(msgBuffer, "%s (%d)", err.message().c_str(), err.value());
+            *errPtr = std::runtime_error(msgBuffer); // NOLINT(cppcoreguidelines-slicing)
         }
     }
 
-    auto StreamControlDaemon::socketRead(boost::asio::ip::tcp::socket& socket) -> GAME_CONTROLLER_BULK {
+    auto StreamControlDaemon::__controlRead(boost::asio::ip::tcp::socket& socket) -> std::tuple<GAME_CONTROLLER_BULK, boost::system::error_code> {
+        GAME_CONTROLLER_BULK result;
         using namespace boost::asio;
-        using ip::tcp;
 
-
+        boost::system::error_code err;
+        read(socket, buffer(&result, sizeof(GAME_CONTROLLER_BULK)), err);
+        return std::make_tuple(result, err);
     }
 
-    auto StreamControlDaemon::socketWrite(boost::asio::ip::tcp::socket &socket,
-            const STREAM_CONTROL_DATA& data) -> void {
+    auto StreamControlDaemon::__controlWrite(boost::asio::ip::tcp::socket& socket,
+                                             const STREAM_CONTROL_DATA& control) -> boost::system::error_code {
         using namespace boost::asio;
-        using ip::udp;
 
-
+        boost::system::error_code err;
+        write(socket, buffer(&control, sizeof(GAME_CONTROLLER_BULK)), err);
+        return err;
     }
 
-    auto StreamControlDaemon::start() -> bool {
-        if (*running) return false;
+    auto StreamControlDaemon::__videoWrite(boost::asio::ip::udp::socket& socket,
+            boost::asio::ip::udp::endpoint& endpoint,
+            const STREAM_CONTROL_DATA& control,
+            const uint8_t* frame) -> boost::system::error_code {
+        using namespace boost::asio;
 
-        *running = true;
-        __workerThread = std::thread(workerMain, this->bot, this->videoPort, this->controlPort, running);
+        auto frameBuffer = buffer(frame, control.frameLength);
+        boost::system::error_code err;
+        socket.send_to(frameBuffer, endpoint, 0, err);
+
+        return err;
+    }
+
+    auto StreamControlDaemon::start() noexcept -> bool {
+        if (*__running) return false;
+
+        *__running = true;
+        __workerThread = std::thread(__workerMain, _bot, _streamPort, _controlPort,
+                __running, _stopReason, _stopException);
 
         return true;
     }
 
-    auto StreamControlDaemon::stop() -> void {
-        *running = false;
+    auto StreamControlDaemon::stop() noexcept -> void {
+        *__running = false;
+        *_stopReason = "Stopped by server.";
     }
 
-    auto StreamControlDaemon::isRunning() -> bool {
-        return *running;
+    auto StreamControlDaemon::isRunning() noexcept -> bool {
+        return *__running;
+    }
+
+    auto StreamControlDaemon::stopReason() noexcept -> std::string {
+        return *_stopReason;
     }
 }
--- src/Control/streamcontroldaemon.hpp
+++ src/Control/streamcontroldaemon.hpp
@@ -7,59 +7,111 @@
 
 #include <thread>
 #include <cstdlib>
-#include <map>
 #include <iostream>
+#include <cmath>
 
-#include "control.hpp"
+#include "robot.hpp"
 #include "streamcontrol.hpp"
 #include "config.hpp"
 #include "gamecontroller.h"
 
 #include <boost/asio.hpp>
+#include <boost/array.hpp>
 
 namespace Control {
     class StreamControlDaemon {
     private:
         std::thread __workerThread;
-        std::shared_ptr<bool> running;
+        std::shared_ptr<bool> __running;
 
-        static auto workerMain(ROBOT_ID bot, uint16_t videoPort, uint16_t controlPort,
-                const std::shared_ptr<bool>& active) -> void;
+        /**
+         * The main worker function for the background thread this class makes.
+         * @param bot A pointer to the robot that is being sent commands.
+         * @param videoPort The UDP port to use when sending the video.
+         * @param controlPort The port to send and receive control data over.
+         * @param active If the thread is active. As soon as this is set false, the worker exits.
+         * @param stopMsg The pointer to the message holding the reason for stopping.
+         * @param errPtr The pointer to a potentially thrown exception.
+         */
+        static auto __workerMain(const std::shared_ptr<Robot>& bot, uint16_t videoPort, uint16_t controlPort,
+                                 const std::shared_ptr<bool>& active, const std::shared_ptr<std::string>& stopMsg,
+                                 const std::shared_ptr<std::exception>& errPtr) -> void;
 
-        static auto socketRead(boost::asio::ip::tcp::socket& socket) -> GAME_CONTROLLER_BULK;
-        static auto socketWrite(boost::asio::ip::tcp::socket& socket, const STREAM_CONTROL_DATA& data) -> void;
+        /**
+         * Reads the game controller data from the socket.
+         * @param socket The socket to read from.
+         * @return The game controller data and any resulting boost error code.
+         */
+        static auto __controlRead(boost::asio::ip::tcp::socket& socket) -> std::tuple<GAME_CONTROLLER_BULK, boost::system::error_code>;
+
+        /**
+         * Writes the stream control data into the socket provided.
+         * @param socket The socket to write data into.
+         * @param control The stream control data to send through the socket.
+         * @return Any resulting boost error code.
+         */
+        static auto __controlWrite(boost::asio::ip::tcp::socket& socket, const STREAM_CONTROL_DATA& control) -> boost::system::error_code;
+
+        /**
+         * Writes the frame data into the socket provided.
+         * @param socket The socket to write data into.
+         * @param endpoint The endpoint to target.
+         * @param control The stream control data to use for frame data formatting.
+         * @param frame The raw tape of the stream frame.
+         * @return Any resulting boost error code.
+         */
+        static auto __videoWrite(boost::asio::ip::udp::socket& socket,
+                boost::asio::ip::udp::endpoint& endpoint,
+                const STREAM_CONTROL_DATA& control,
+                const uint8_t* frame) -> boost::system::error_code;
+
     protected:
-        const uint16_t videoPort;
-        const uint16_t controlPort;
-        const ROBOT_ID bot;
+        const uint16_t _streamPort;
+        const uint16_t _controlPort;
+
+        std::shared_ptr<Robot> _bot;
+
+        std::shared_ptr<std::string> _stopReason = std::make_shared<std::string>("Not yet started.");
+        std::shared_ptr<std::exception> _stopException;
 
     public:
         /**
-         * Creates the daemon.
-         * @param id The id of the robot to send commands to.
-         * @param port The networking port to use for the stream communication.
+         * Constructs the daemon.
+         * @param bot The _bot that should be controlled through this daemon.
+         * @param streamPort The port to send the stream data through (UDP).
+         * @param controlPort The port to send and receive control data through (TCP).
          * @param start Start the daemon on construction.
          */
-        explicit StreamControlDaemon(ROBOT_ID id, uint16_t videoPort = REMOTE_VIDEO_PORT,
-                uint16_t controlPort = REMOTE_CONTROL_PORT, bool start = false);
+        explicit StreamControlDaemon(const std::shared_ptr<Robot>& bot, uint16_t streamPort = Config::REMOTE_STREAM_PORT,
+                uint16_t controlPort = Config::REMOTE_CONTROL_PORT, bool start = false);
+
+        /**
+         * Deconstructs and stops the daemon.
+         */
         ~StreamControlDaemon();
 
         /**
          * Starts the daemon if not started already.
          * @return If the thread started on this method call.
          */
-        auto start() -> bool;
+        auto start() noexcept -> bool;
 
         /**
          * Stops the daemon.
          */
-        auto stop() -> void;
+        auto stop() noexcept -> void;
 
         /**
-         * Checks if the daemon is running.
-         * @return If the daemon is running.
+         * Checks if the daemon is __running.
+         * @return If the daemon is __running.
          */
-        auto isRunning() -> bool;
+        auto isRunning() noexcept -> bool;
+
+        /**
+         * Gets the reason for the thread stopping.
+         * @return The reason for the daemon stopping.
+         */
+        auto stopReason() noexcept -> std::string;
     };
 }
 
