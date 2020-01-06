c120de4515496f55b47c659ef6ccafa23d41d82b
James McRoberts <jamesmcroberts82997@gmail.com>
2020-01-06 02:46:36 +0000

    Created main header

---
+++ src/main.hpp
@@ -0,0 +1,33 @@
+//
+// Created by james on 1/5/20.
+//
+
+#ifndef BEHOLDER_MAIN_HPP
+#define BEHOLDER_MAIN_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <memory>
+
+#include "robot.hpp"
+#include "config.hpp"
+
+struct inputs {
+    std::unique_ptr<std::string> serialPath;
+    std::unique_ptr<int> baud;
+    std::unique_ptr<uint> responseTime;
+    std::unique_ptr<char> stateChar;
+    std::unique_ptr<char> testChar;
+    std::unique_ptr<uint16_t> controlPort;
+    std::unique_ptr<uint16_t> streamPort;
+    std::unique_ptr<float> speed;
+    std::unique_ptr<float> threshold;
+};
+
+auto printVersion() -> void;
+auto printHelp() -> void;
+auto compileArguments(int argc, char* argv[]) -> struct inputs;
+auto processArguments(struct inputs* arguments) -> int;
+
+#endif //BEHOLDER_MAIN_HPP
